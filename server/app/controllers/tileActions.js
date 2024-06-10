da1943b214c0f1e260acf4540446ac6dc12a129d
ileanaspl <ileana.saintpaul@gmail.com>
2024-06-10 11:40:53 +0200

    finish step 2

--- server/app/controllers/tileActions.js
+++ server/app/controllers/tileActions.js
@@ -1,1 +1,13 @@
-module.exports = {};
+const tables = require("../../database/tables");
+
+const browse = async (req, res, next) => {
+  try {
+    const tiles = await tables.tile.readAll();
+
+    res.json(tiles);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { browse };
