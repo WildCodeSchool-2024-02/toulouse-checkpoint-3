3f93111a313c847012b02e59b0257dcfc86717a8
BrebG <brebelgael@protonmail.com>
2024-06-10 11:52:24 +0200

    feat: step5

--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -7,7 +7,19 @@ class BoatRepository extends AbstractRepository {
 
   async readAll() {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await this.database.query(`select * from ${this.table}`);
+    const [rows] = await this.database.query(
+      `SELECT
+      boat.id,
+      boat.name,
+      boat.coord_x,
+      boat.coord_y,
+      tile.type,
+      tile.coord_x,
+      tile.coord_y,
+      tile.has_treasure
+    FROM boat
+    JOIN tile ON boat.coord_x = tile.coord_x AND boat.coord_y = tile.coord_y`
+    );
 
     // Return the array of boats
     return rows;
